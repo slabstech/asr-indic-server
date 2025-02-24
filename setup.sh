c59b8f8099a77eb73c460f04300df942a0305c84
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-02-24 17:52:43 +0100

    fixes

---
+++ setup.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install -y python3-venv
+sudo apt-get install -y python3-pip
+
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+uvicorn src.asr_indic_server.asr_api:app --reload
+
